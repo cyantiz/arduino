d50aa126f49c19b1ef29b0609f7c49ba1788a4c6
cyantiz <nhan053qt@gmail.com>
2022-12-08 17:51:43 +0700

    fix: servo

--- servo_motor/servo_motor.ino
+++ servo_motor/servo_motor.ino
@@ -1,19 +1,21 @@
 #include <Servo.h>
 Servo myservo;
-int potpin = 0;
+int potPin = 0;
 int val;
 int toneVal;
 float sinVal;
+int servo = 9;
+int piezo = 8;
+
 
 void setup() {
-  myservo.attach(9);
+  myservo.attach(servo);
   Serial.begin(9600);
-  pinMode(8, OUTPUT);
+  pinMode(piezo, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  val = analogRead(potpin);
+  val = analogRead(potPin);
 
   val = map(val, 0, 1023, 0, 180);
 
@@ -21,19 +23,19 @@ void loop() {
   Serial.println(val);
 
   if (val > 90) {
-    piezoSound();  
+    piezoSound(piezo);  
   } else {
-    noTone(8);  
+    noTone(piezo);  
   }
 
   delay(15);
   
 }
 
-void piezoSound() {
+void piezoSound(int piezoPin) {
   for (int i = 0; i < 180; i++){
       sinVal = sin(i * 3.1412 / 180);
       toneVal = int(sinVal * 1000) + 2000;
-      tone(8, toneVal);
+      tone(piezoPin, toneVal);
   }
 }
